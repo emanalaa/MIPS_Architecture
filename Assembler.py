9243cc2520ca6253bbb2003a535ee93111a6edcc
karim tarek <karimtarekk74@gmail.com>
2019-03-29 20:53:29 +0200

    Created instruction map.

--- Assembler.py
+++ Assembler.py
@@ -4,7 +4,7 @@ def to_binary():
 
 class MipsAssembler:
     def __init__(self):
-        pass
+        self.map_instructions = dict()
 
     def read_file(self):
         pass
@@ -19,7 +19,19 @@ class MipsAssembler:
         pass
 
     def instructions_mapping(self):
-        pass
+        self.map_instructions = {
+         "add": ["000000", "100000", "R"],
+         "and": ["000000", "10100", "R"],
+         "sub": ["000000", "100010", "R"],
+         "nor": ["000000", "100111", "R"],
+         "or": ["000000", "100101", "R"],
+         "slt": ["000000", "101010", "R"],
+         "addi": ["001000", "none", "I"],
+         "lw": ["100011", "none", "I"],
+         "sw": ["101011", "none", "I"],
+         "beq": ["000100", "none", "I"],
+         "bne": ["000101", "none", "I"],
+         "j": ["000010", "none", "J"]}
 
     def assembler(self):
         pass

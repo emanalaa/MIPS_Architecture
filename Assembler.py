af809c7d2720718913032bdebdfbadff15d6deb2
MennatAllahMagdy <mennamagdykh@gmail.com>
2019-03-29 20:53:28 +0200

    Created registers map.

--- Assembler.py
+++ Assembler.py
@@ -3,8 +3,8 @@ def to_binary():
 
 
 class MipsAssembler:
-    def __init__(self):
-        pass
+    def __init__(self,reg_map):
+        self.reg_map=dict(reg_map)
 
     def read_file(self):
         pass
@@ -16,7 +16,40 @@ class MipsAssembler:
         pass
 
     def registers_mapping(self):
-        pass
+        reg_map = {
+          "$zero": "00000",
+          "$at": "00001",
+          "$v0": "00010",
+          "$v1": "00011",
+          "$a0": "00100",
+          "$a1": "00101",
+          "$a2": "00110",
+          "$a3": "00111",
+          "$t0": "01000",
+          "$t1": "01001",
+          "$t2": "01010",
+          "$t3": "01011",
+          "$t4": "01100",
+          "$t5": "01101",
+          "$t6": "01110",
+          "$t7": "01111",
+          "$t8": "11000",
+          "$t9": "11001",
+          "$s0": "10000",
+          "$s1": "10001",
+          "$s2": "10010",
+          "$s3": "10011",
+          "$s4": "10100",
+          "$s5": "10101",
+          "$s6": "10110",
+          "$s7": "10111",
+          "$k0": "11010",
+          "$k1": "11011",
+          "$gp": "11100",
+          "$sp": "11101",
+          "fp": "11110",
+          "$ra": "11111"
+        }
 
     def instructions_mapping(self):
         pass

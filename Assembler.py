392681033c0b91c76a113722ab973dad12201635
emanalaa <eman3laa@outlook.com>
2019-03-29 19:18:37 +0200

    Created initial class for the assembler.

---
+++ Assembler.py
@@ -0,0 +1,27 @@
+def to_binary():
+    pass
+
+
+class MipsAssembler:
+    def __init__(self):
+        pass
+
+    def read_file(self):
+        pass
+
+    def write_file(self):
+        pass
+
+    def labels_mapping(self):
+        pass
+
+    def registers_mapping(self):
+        pass
+
+    def instructions_mapping(self):
+        pass
+
+    def assembler(self):
+        pass
+
+
